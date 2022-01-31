9d32430df0de1e4a029b7f3b8d4e3f6fba8f4166
Joonha Kim <joonhakim1@gmail.com>
2022-01-31 09:27:48 -0600

    Finished all the requirements for the assignment.

--- rubik/check.py
+++ rubik/check.py
@@ -1,10 +1,100 @@
 import rubik.cube as rubik
+from pickle import TRUE
 
 def _check(parms):
     result={}
-    encodedCube = parms.get('cube',None)       
-    if(encodedCube == None):
-        result['status'] = 'error: xxx'
+    
+    
+    
+    '''
+    is present (V)
+    is a string (V)
+    has 54 elements (V)
+    has 9 occurrences of 6 colors (V)
+    has each middle face being a different color
+    '''
+    encodedCube = parms.get('cube', None)   
+   
+    
+    
+    # using only for testing occurrences of color
+    count={}
+    
+
+           
+
+    
+    # <class 'NoneType'>
+    # <class 'int'>
+
+    #Type test 
+        
+    if (encodedCube == None):
+        result['status'] = 'error: cube is none'
+        return result
+    
+    elif (encodedCube == ''):
+        result['status'] = 'error: there is empty cube input'
+        return result
+    
+    elif ((str(type(encodedCube)) != "<class 'str'>")):
+        result['status'] = 'error: Invalid cube input type'
+        return result
+    
     else:
-        result['status'] = 'a-ok'
+        lists = list(encodedCube) 
+    
+
+    # lists = list(encodedCube.replace(" ", ""))
+    #
+
+    # print(lists)
+    # print(count)
+    #
+    # toF = False
+
+    # using only for testing occurrences of color
+    numCnt = False
+    
+    for i in lists:
+        
+        try: count[i] += 1
+        except: count[i]=1
+              
+    for j in count.values():
+        if (9 != j):
+            numCnt = True
+            break
+    
+    
+    k = 4
+    l = k + 9
+    faceCheck = False
+    
+    while k <= len(lists):
+        while l < len(lists):
+            if lists[k] == lists[l]:
+                faceCheck = True
+            l += 9
+        l = k + 18
+        k += 9    
+
+
+
+    
+    if(len(lists) != 54):
+        result['status'] = 'error: Invalid number of total cube elements' 
+        
+    elif(len(count) != 6):
+        result['status'] = 'error: Cube does not have 6 colors.'
+          
+    elif(numCnt):
+        result['status'] = 'error: Some cube color may not have 9 occurrences'  
+        
+    elif(faceCheck):
+        result['status'] = 'error: Invalid cube middle face color'  
+
+    else:
+        result['status'] = 'ok'
+   
     return result
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -2,12 +2,179 @@ from unittest import TestCase
 import rubik.check as check 
 
 class CheckTest(TestCase):
-        
+
     def test_check_010_ShouldReturnOkOnSolvedCube(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
         result = check._check(parm)
         self.assertIn('status', result)
-        status = result.get('status', None)
+        status = result.get('status')
         self.assertEqual(status, 'ok')
 
+    def test_check_020_ShouldReturnErrorOnNone(self):
+        parm = {'op':'check',
+                'cube':None}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: cube is none')
+
+    def test_check_030_ShouldReturnErrorOnEmpty(self):
+        parm = {'op':'check',
+                'cube': ''}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: there is empty cube input')
+
+    def test_check_040_ShouldReturnErrorOnWrongType(self):
+        parm = {'op':'check',
+                'cube': 12345}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: Invalid cube input type')
+
+    def test_check_050_ShouldReturnErrorOnTotalNumber(self):
+        parm = {'op':'check',
+                'cube': 'bbbbbbbbbbrrrrrrrrrrggggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: Invalid number of total cube elements')
+
+    def test_check_060_ShouldReturnErrorOnNumberElement(self):
+        parm = {'op':'check',
+                'cube': 'bbbbbbbbbrrrrrvrrrgggzgggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: Cube does not have 6 colors.')
+
+    def test_check_070_ShouldReturnErrorOnOccurrence(self):
+        parm = {'op':'check',
+                'cube': 'bbbbbbbbbbrrrrrrrrggggggggggooooooooyyyyyyyywwwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: Some cube color may not have 9 occurrences')
+        
+    def test_check_080_ShouldReturnErrorOnMiddleFace(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbrbrrrrbrrrrggggoggggogoooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: Invalid cube middle face color')
+        
+    def test_check_090_ShouldReturnOkOnCube(self):
+        parm = {'op':'check',
+                'cube':'11w11w11wrrrrrrrrryggyggyggaaaaaaaaayy1yy1yy1wwgwwgwwg'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'ok')
+        
+    def test_check_010_ShouldReturnErrorOnShortCube(self):
+        parm = {'op':'check',
+                'cube':'123456789'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: Invalid number of total cube elements')    
+
+    def test_check_011_ShouldReturnErrorOnNoneCube(self):
+        parm = {'op':'check',
+                }
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: cube is none') 
+        
+    def test_check_011_ShouldReturnErrorOnNoneCube(self):
+        parm = {'op':'check',
+                'cube' : 42}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status')
+        self.assertEqual(status, 'error: Invalid cube input type')   
+        
+    # def test_check_020_ShouldReturnErrorOnEmptyCube(self):
+    #     parm = {'op':'check',
+    #             'cube': None}
+    #     result = check._check(parm)
+    #     self.assertIn('status', result)
+    #     status = result.get('status')
+    #     self.assertEqual(status, 'error: cube is none')
+    #
+    # def test_check_030_ShouldReturnErrorOnNoCube(self):
+    #     parm = {'op':'check',
+    #             'cube': ''}
+    #     result = check._check(parm)
+    #     self.assertIn('status', result)
+    #     status = result.get('status')
+    #     self.assertEqual(status, 'error: there is empty cube input')
+    #
+    # def test_check_040_ShouldReturnErrorOnShortCube(self):
+    #     parm = {'op':'check',
+    #             'cube': '                                                      '}
+    #     result = check._check(parm)
+    #     self.assertIn('status', result)
+    #     status = result.get('status')
+    #     self.assertEqual(status, 'error: Invalid number of total cube elements')
+    #
+    # def test_check_041_ShouldReturnErrorOnShortCube(self):
+    #     parm = {'op':'check',
+    #             'cube': '                                                      abc'}
+    #     result = check._check(parm)
+    #     self.assertIn('status', result)
+    #     status = result.get('status')
+    #     self.assertEqual(status, 'error: Invalid number of total cube elements')
+    #
+    # def test_check_050_ShouldReturnOkOnCubeWithSpace(self):
+    #     parm = {'op':'check',
+    #             'cube': '                                                      bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+    #     result = check._check(parm)
+    #     self.assertIn('status', result)
+    #     status = result.get('status')
+    #     self.assertEqual(status, 'error: Invalid number of cube colors')       
+    #
+    # def test_check_051_ShouldReturnOkOnCubeWithSpace(self):
+    #     parm = {'op':'check',
+    #             'cube': '                                                      b b b b b b b b b r r r r r r r r r g g g g g g g g g o o oo o o o o o y y y yy y y y y w w w w w w w w w'}
+    #     result = check._check(parm)
+    #     self.assertIn('status', result)
+    #     status = result.get('status')
+    #     self.assertEqual(status, 'ok')
+    #
+    # def test_check_060_ShouldReturnErrorOnInvalidCubeType(self):
+    #     parm = {'op':'check',
+    #             'cube': 34567}
+    #     result = check._check(parm)
+    #     self.assertIn('status', result)
+    #     status = result.get('status')
+    #     self.assertEqual(status, 'error: Invalid cube input type')
+    #
+    # def test_check_070_ShouldReturnErrorOnInvalidCubeColor(self):
+    #     parm = {'op':'check',
+    #             'cube': 'bbbbbbbbbrrrrrrrrraaaggggggoobbbooooyyccyyyyywwwzwwwww'}
+    #     result = check._check(parm)
+    #     self.assertIn('status', result)
+    #     status = result.get('status')
+    #     self.assertEqual(status, 'error: Invalid number of cube colors')
+    #
+    # def test_check_080_ShouldReturnErrorOnInvalidCubeMiddle(self):
+    #     parm = {'op':'check',
+    #             'cube': 'bbbbrbbbbrrrrrrrrbgggggggggoooooooooyyyyyyyywwwwwywwww'}
+    #     result = check._check(parm)
+    #     self.assertIn('status', result)
+    #     status = result.get('status')
+    #     self.assertEqual(status, 'error: Invalid cube middle face color')
+    #
+    # def test_check_081_ShouldReturnErrorOnInvalidCubeMiddle(self):
+    #     parm = {'op':'check',
+    #             'cube': 'bbb b r b b b b  r r r r r rrrbgggggggggoooooooooyyyyyyyyww w w w y w w ww'}
+    #     result = check._check(parm)
+    #     self.assertIn('status', result)
+    #     status = result.get('status')
+    #     self.assertEqual(status, 'error: Invalid number of cube colors')
